5109996927a87d63784ca82ace4e1056a533d1ae
cahya dsn <cahyadsn@gmail.com>
2025-03-14 08:32:56 +0700

    update2503140822

--- index.php
+++ index.php
@@ -6,17 +6,22 @@
 FILENAME     : index.php
 AUTHOR       : CAHYA DSN
 CREATED DATE : 2025-02-02 17:08:09
-UPDATED DATE : 2025-02-03 17:30:19
+UPDATED DATE : 2025-03-14 08:32:22
 DEMO SITE    : http://psycho.cahyadsn.com/bfi
 SOURCE CODE  : https://github.com/cahyadsn/bfi
 ================================================================================
 This program is free software; you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later version.
+terms of the MIT License.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+See the MIT License for more details
 
 copyright (c) 2025 by cahya dsn; cahyadsn@gmail.com
 ================================================================================ */
